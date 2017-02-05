08a767f3c6d2725da380734c79b572b324446952
allenwwg <allenwwg@qq.com>
2017-02-05 22:39:37 +0800

    vtMain_cmd

--- vn.trader/vtMain_cmd.py
+++ vn.trader/vtMain_cmd.py
@@ -14,17 +14,18 @@ class EventLoopManager(object):
         self.engineStarted = False
         self.mainEngine = MainEngine()
         self.eventEngine = EventEngine2()
-        print "connect MongoDB.........."
+	
+        # 注册事件监听
+        self.registerEvent()
         self.mainEngine.dbConnect()
-        time.sleep(5)
-    
-        print "connet CTP.........."
+        time.sleep(2)
+	'''
         self.mainEngine.connect('CTP')
-        time.sleep(5)
-    
-        print "start strategy double ema......."
+        time.sleep(3)
         self.mainEngine.ctaEngine.loadSetting()
         self.mainEngine.ctaEngine.initStrategy('double ema')
+	'''
+        self.eventEngine.start()
         
     def start(self):
         self.eventEngine.start()
@@ -33,7 +34,7 @@ class EventLoopManager(object):
         
     def onEventLog(self, event):
         log = event.dict_['data']
-        logContent = ''.join([log.logTime, log.logContent, '\n'])
+        logContent = ' '.join([log.logTime, log.logContent, '\n'])
         self.makeLog(logContent)
         
     def onEventTimer(self, event):
@@ -46,18 +47,16 @@ class EventLoopManager(object):
         if(self.engineStarted == False 
            and (hour in [9, 10, 11, 13, 20, 21, 22, 23, 24, 1])#trading hour
            ):
-            self.engineStarted = True
-            self.makeLog('Starting CTP double ema')
+            #self.makeLog('Starting CTP double ema')
             self.run()
             return
-        
+        time.sleep(2)
         #log out
         if(self.engineStarted == True 
            and (hour in [14, 2])
            ):
-            self.makeLog('Ending CTP double ema')
+            #self.makeLog('Ending CTP double ema')
             self.stop()
-            self.engineStarted = False
             return        
         
     def registerEvent(self):
@@ -65,16 +64,24 @@ class EventLoopManager(object):
         self.mainEngine.eventEngine.register(EVENT_LOG, self.onEventLog)
         self.mainEngine.eventEngine.register(EVENT_CTA_LOG, self.onEventLog)
         self.eventEngine.register(EVENT_TIMER, self.onEventTimer)
-        self.makeLog('Events registerred',False)
+        #self.makeLog('Events registerred',False)
         
     def run(self):
+        self.mainEngine.connect('CTP')
+        time.sleep(3)
+
+        self.mainEngine.drEngine.loadSetting()
+
+        self.mainEngine.ctaEngine.loadSetting()
+        self.mainEngine.ctaEngine.initStrategy('double ema')
         self.mainEngine.ctaEngine.startStrategy('double ema')
         self.engineStarted = True
         
     def stop(self):
         try:
-            self.mainEngine.ctaEngine.stopStrategy('double ema')
+            #self.mainEngine.ctaEngine.stopStrategy('double ema')
             #self.mainEngine.exit()
+ 	        #time.sleep(2)
             self.engineStarted = False
         except Exception as e:
             print e
@@ -87,4 +94,4 @@ class EventLoopManager(object):
 if __name__ == '__main__':
     import time
     manager = EventLoopManager()
-    manager.start()
+    #manager.start()
